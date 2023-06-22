5f975a2d834058dbd977e2961b1fe3704a318502
Br1-O <bruno.ortuno2@gmail.com>
2023-06-22 06:19:21 -0300

    improved form and carousel incorporated

--- app/View/Index.php
+++ app/View/Index.php
@@ -8,17 +8,17 @@
 
 <!-- ■■■■■■■■ NavBar ■■■■■■■■-->  
 
-  <nav class="navbar-container">
-    <div class="navbar">
+  <nav class="navbar-container1">
+    <div class="navbar1">
         
-      <img class='logo' src="../../public/images/batch_logo.png" alt="logo Batch Tramites y Procesos" aria-label="logo Batch Tramites y Procesos">
+      <img class='logo1' src="../../public/images/batch_logo.png" alt="logo Batch Tramites y Procesos" aria-label="logo Batch Tramites y Procesos">
       
-      <div class='nav-list'>
-        <ul class='nav-list'>
-          <li class='nav-item'><a href="#">  Inicio</a></li>
-          <li class='nav-item'><a href="#about-us">  Quienes somos  </a></li>
-          <li class='nav-item'><a href="#services">  Servicios  </a></li>
-          <li class='nav-item'><a href="#contact">  Contacto  </a></li>
+      <div class='nav-list1'>
+        <ul class='nav-list1'>
+          <li class='nav-item1'><a href="#">  Inicio</a></li>
+          <li class='nav-item1'><a href="#about-us">  Quienes somos  </a></li>
+          <li class='nav-item1'><a href="#services">  Servicios  </a></li>
+          <li class='nav-item1'><a href="#contact">  Contacto  </a></li>
         </ul>
       </div>
 
@@ -31,12 +31,12 @@
 
     <!-- ■■■■■■■■ Header ■■■■■■■■-->  
   
-      <section class="blueprint-container">
-        <div class="blueprint">
+      <section class="blueprint-container1">
+        <div class="blueprint1">
 
-          <h1 class='tittle'> Tus trámites y procesos, de manera simple </h1>
+          <h1 class='tittle1'> Tus trámites y procesos, de manera simple </h1>
           <p>Occaecat est ipsum reprehenderit reprehenderit veniam anim laborum est esse duis occaecat reprehenderit pariatur.</p>
-          <button class='btn-blueprint'> Empieza ahora </button>
+          <button class='btn-blueprint1'> Empieza ahora </button>
           <a href="#">Conoce más</a>
         
         </div>
@@ -44,19 +44,19 @@
 
     <!-- ■■■■■■■■ Main ■■■■■■■■-->  
 
-      <section class="solutions-container">
+      <section class="solutions-container1">
         
         <article>
 
           <h2> 4 soluciones diseñadas para <u>simplificar tus procesos</u> </h2>
 
-          <div class="even-columns">
+          <div class="even-columns1">
             
-            <div class='split-image'>
+            <div class='split-image1'>
               <img src="../../public/images/splash-solutions.png" alt="splash de edificio visto desde abajo" aria-label="imagen splash de edificio visto desde abajo">
             </div>
             
-            <div class="grid">
+            <div class="grid1">
 
                 <div class="ag-offer-block">
               <div class="ag-format-container">
@@ -131,26 +131,26 @@
 
         </article>
 
-        <article class='flame-wave-container overlay-container'>
+        <article class='flame-wave-container1 overlay-container1'>
 
           <h2> Empresas que <u>nos respaldan</u> </h2>
 
-          <div class="even-columns">
+          <div class="even-columns1">
 
-            <img id='flame-solutions' src="../../public/images/flame_solutions.png" alt="logotipo de flama blanco y negro">
+            <img id='flame-solutions1' src="../../public/images/flame_solutions.png" alt="logotipo de flama blanco y negro">
 
-            <div class='grid-icons'>
-              <span class="icon-circle">
-                <a href="#" class="circle-anchor"></a>
+            <div class='grid-icons1'>
+              <span class="icon-circle1">
+                <a href="#" class="circle-anchor1"></a>
               </span>
-              <span class="icon-circle">
-                <a href="#" class="circle-anchor"></a>
+              <span class="icon-circle1">
+                <a href="#" class="circle-anchor1"></a>
               </span>
               <span class="icon-circle2">
-                <a href="#" class="circle-anchor"></a>
+                <a href="#" class="circle-anchor1"></a>
               </span>
               <span class="icon-circle3">
-                <a href="#" class="circle-anchor"></a>
+                <a href="#" class="circle-anchor1"></a>
               </span>
             </div>
           </div>
@@ -162,66 +162,123 @@
       </section>
 
     <!-- ■■■■■■■■ Carrousel ■■■■■■■■-->  
-      <section class="carrousel">
-        <div>
 
+      <section class='opinions-section'>
+
+        <h2> Lo que dicen de nosotros </h2>
+
+        <div class="carrousel1">
+            
+          <div id="testimonios-carousel" class="carousel carousel-dark slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+
+              <div class="carousel-item active">
+                <div class="container">
+                  <img class="testimonio-imagen rounded-circle" src="../../public/images/Recomendación ejemplo.png" alt="foto de cliente 1">
+                  <p class="testimonio-texto english">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum rhonelibero tellus. Don ornare magna. Aenean nisi ante, finibus non faucibus a, volutpat vel lorem. Morbi nec commodo tortor. Duis vulputate tempor mauris sed ultrices. </p>
+                  <div class="testimonio-info">
+                    <p class="nombre_cliente">Gino</p>
+                    <p class="cargo_cliente english">Manager of Constructora S.A. 1</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="carousel-item">
+                <div class="container">
+                  <img class="testimonio-imagen rounded-circle" src="../../public/images/Recomendación ejemplo.png" alt="foto de cliente 2">
+                  <p class="testimonio-texto english">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum rhonelibero tellus. Don ornare magna. Aenean nisi ante, finibus non faucibus a, volutpat vel lorem. Morbi nec commodo tortor. Duis vulputate tempor mauris sed ultrices. </p>
+                  <div class="testimonio-info">
+                    <p class="nombre_cliente">Nora</p>
+                    <p class="cargo_cliente english">Manager of Constructora S.A. 2</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="carousel-item">
+                <div class="container">
+                  <img class="testimonio-imagen rounded-circle" src="../../public/images/Recomendación ejemplo.png" alt="foto de cliente 3">
+                  <p class="testimonio-texto english">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum rhonelibero tellus. Don ornare magna. Aenean nisi ante, finibus non faucibus a, volutpat vel lorem. Morbi nec commodo tortor. Duis vulputate tempor mauris sed ultrices. </p>
+                  <div class="testimonio-info">
+                    <p class="nombre_cliente">Leonardo</p>
+                    <p class="cargo_cliente english">Director of Constructora S.A. 3</p>
+                  </div>
+                </div>
+              </div>
+        
+            </div>
+
+            <button class="carousel-control-prev" type="button" data-bs-target="#testimonios-carousel" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden english">Previous</span>
+            </button>
+
+            <button class="carousel-control-next" type="button" data-bs-target="#testimonios-carousel" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden english">Next</span>
+            </button>
+
+          </div>
         </div>
-      </section>
+      </div>
+
+    </section>
     
     <!-- ■■■■■■■■ Form ■■■■■■■■-->  
 
-      <section class="form-container">
+      <section class="form-container1">
 
         <h2>Solicita más información</h2>
 
-        <form class='even-columns form' action="../Controller/insert_consultation" method='POST'>
+        <form class='even-columns1 form1' action="../Controller/insert_consultation" method='POST'>
 
           <div class="form-col1">
-          <label class='form-label' for="name">*Nombre de la empresa</label>
-          <input class='form-input' type="text" name='name' placeholder="Escribe aquí el nombre de tu empresa">
+          <label class='form-label1' for="name">*Nombre de la empresa</label>
+          <input class='form-input1' type="text" name='name' placeholder="Escribe aquí el nombre de tu empresa">
 
-          <label class='form-label' for="nameResponsible">*Nombre completo</label>
-          <input class='form-input' type="text" name='nameResponsible' placeholder="Escribe aquí tu nombre completo">
+          <label class='form-label1' for="nameResponsible">*Nombre completo</label>
+          <input class='form-input1' type="text" name='nameResponsible' placeholder="Escribe aquí tu nombre completo">
 
-          <label class='form-label' for="positionResponsible">*Cargo</label>
-          <input class='form-input' type="text" name='positionResponsible' placeholder="Escribe aquí tu cargo">
+          <label class='form-label1' for="positionResponsible">*Cargo</label>
+          <input class='form-input1' type="text" name='positionResponsible' placeholder="Escribe aquí tu cargo">
 
-          <label class='form-label' for="phoneResponsible">*Teléfono</label>
-          <input class='form-input' type="text" name='phoneResponsible' placeholder="Escribe aquí tu teléfono">
+          <label class='form-label1' for="phoneResponsible">*Teléfono</label>
+          <input class='form-input1' type="text" name='phoneResponsible' placeholder="Escribe aquí tu teléfono">
 
-          <label class='form-label' for="emailResponsible">*Correo electrónico</label>
-          <input class='form-input' type="text" name='emailResponsible' placeholder="Escribe aquí tu correo">
+          <label class='form-label1' for="emailResponsible">*Correo electrónico</label>
+          <input class='form-input1' type="text" name='emailResponsible' placeholder="Escribe aquí tu correo">
           </div>
 
           <div class="form-col2">
-            <div class='radio-container'>
-              <input class='form-radio' type="radio" name='request'>
-              <label class='form-label radio-label' >Quiero pedir un presupuesto</label>
+            <div class='radio-container1'>
+              <input class='form-radio1' type="radio" name='request'>
+              <label class='form-label1 radio-label1' >Quiero pedir un presupuesto</label>
             </div>
 
-            <div class='radio-container'>
-              <input class='form-radio' type="radio" name='request'>
-              <label class='form-label radio-label' >Quiero hacer una consulta</label>
+            <div class='radio-container1'>
+              <input class='form-radio1' type="radio" name='request'>
+              <label class='form-label1 radio-label' >Quiero hacer una consulta</label>
             </div>
 
-          <label class='form-label text-area-label' for="">*Favor indicar...</label>
-          <input id='text-area' class='form-input' type="textarea">
+          <label class='form-label1 text-area-label1' for="">*Favor indicar...</label>
+          <textarea name="message" id='text-area' class='form-input1' cols="50" rows="40" placeholder="Por favor, escribe aquí tu consulta..."></textarea>
 
-          <button>Enviar</button>
+          <button>Enviar <i class="bi bi-send"></i></button>
           </div>
           
         </form>
       </section>
 
   </main>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 
   <!-- ■■■■■■■■ Footer ■■■■■■■■-->  
 
-    <footer class='footer'>
+    <footer class='footer1'>
 
-      <div class="footer-container even-columns">
-        <div class='nav-footer'>
+      <div class="footer-container1 even-columns1">
+        <div class='nav-footer1'>
           <h3>Nuestra empresa</h3>
           <ul>
             <li>Sobre nosotros</li>
